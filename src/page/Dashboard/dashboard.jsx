99ed703cfe095cad74ddcef8c189ac06e0fcce5f
N-ntw <nealyl.wang@mail.utoronto.ca>
2023-07-26 02:45:54 -0400

    Dashboard with information part

--- src/page/Dashboard/dashboard.jsx
+++ src/page/Dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axiosInstance from '../../request/index.jsx';
 import {
   DesktopOutlined,
   FileOutlined,
@@ -17,7 +18,7 @@ function getItem(label, key, icon, children) {
   };
 }
 const items = [
-  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Information', '1', <PieChartOutlined />),
   getItem('Option 2', '2', <DesktopOutlined />),
   getItem('User', 'sub1', <UserOutlined />, [
     getItem('Tom', '3'),
@@ -25,61 +26,58 @@ const items = [
     getItem('Alex', '5'),
   ]),
   getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  getItem('Files', '9', <FileOutlined />),
 ];
 
 
 const Dashboard = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const fetchUserInfo = () => {
+    // Replace 'userId' with the actual ID of the current user
+    axiosInstance.get(`/users/4`)
+      .then(response => {
+        setUser(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Failed to fetch user information:', error);
+      });
+  }
+  // Fetch user information on component mount
+  useEffect(() => {
+    fetchUserInfo();
+  }, []);
+ 
   const {
     token: { colorBgContainer },
   } = theme.useToken();
   return (
-    <Layout
-      style={{
-        minHeight: '100vh',
-      }}
-    >
+    <Layout style={{ minHeight: '100vh' }}>
       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
         <div className="demo-logo-vertical" />
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
       </Sider>
       <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        />
-        <Content
-          style={{
-            margin: '0 16px',
-          }}
-        >
-          <Breadcrumb
-            style={{
-              margin: '16px 0',
-            }}
-          >
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
             <Breadcrumb.Item>User</Breadcrumb.Item>
             <Breadcrumb.Item>Bill</Breadcrumb.Item>
           </Breadcrumb>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: colorBgContainer,
-            }}
-          >
-            Bill is a cat.
+          <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
+            {loading ? (
+            'Loading user information...'
+            ) : (
+            <div>
+                <h2>{user && user.id}</h2>
+                <p>{user && user.email}</p>
+            </div>
+            )}
           </div>
         </Content>
-        <Footer
-          style={{
-            textAlign: 'center',
-          }}
-        >
-          Ant Design ©2023 Created by Ant UED
+        <Footer style={{ textAlign: 'center' }}>
+          Your Footer Here
         </Footer>
       </Layout>
     </Layout>
