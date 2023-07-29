4e33a92866f9bbb95f10554b44890f05eb0db926
N-ntw <nealyl.wang@mail.utoronto.ca>
2023-07-29 02:13:36 -0400

    Video React page, no server and database setup

--- src/page/EditVideo/EditVideo.jsx
+++ src/page/EditVideo/EditVideo.jsx
@@ -1,122 +1,117 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Form, Input, List, Upload } from 'antd';
+import { Button, Form, Input, List, Upload, message, notification, Image } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
-import  axiosInstance  from '../../request/index.jsx';
+import axiosInstance from '../../request/index.jsx';
 
-const Paintings = () => {
-  const [paintings, setPaintings] = useState([]);
+const Videos = () => {
+  const [videos, setVideos] = useState([]);
   const [fileList, setFileList] = useState([]);
   const [editingId, setEditingId] = useState(null);
   const [editingFileList, setEditingFileList] = useState([]);
   const [form] = Form.useForm();
   const [editingForm] = Form.useForm();
 
-  
-  const handleUpload = ({fileList}) => {
+  const handleUpload = ({ fileList }) => {
     setFileList(fileList);
-    }
+  }
 
-  const handleEditUpload = ({fileList}) => {
+  const handleEditUpload = ({ fileList }) => {
     setEditingFileList(fileList);
   }
 
-  const fetchPaintings = () => {
-    // Fetch the paintings from your server and update the state
-    // You need to implement this function in your server code
-
-
-
-    axiosInstance.get(`/users/4/paintings`)
+  const openNotification = (type, message) => {
+    notification[type]({
+      message: message,
+    })
+  }
+  
+  const fetchVideos = () => {
+    axiosInstance.get(`/users/4/videos`)
       .then(response => {
-        setPaintings(response.data);
+        setVideos(response.data);
       })
       .catch(error => {
-        console.error('Failed to fetch paintings:', error);
+        console.error('Failed to fetch videos:', error);
       });
   }
 
-  const addPainting = (values) => {
-    // Send a POST request to your server to add a new painting
-    // You need to implement this function in your server code
+  const addVideo = (values) => {
     const formData = new FormData();
     formData.append('title', values.title);
     formData.append('description', values.description);
-    // Add the file to the request body
     if (fileList.length > 0) {
-        formData.append('image', fileList[0].originFileObj);
+        formData.append('video', fileList[0].originFileObj);
     }
-    axiosInstance.post(`/users/4/paintings`, formData, {
+    axiosInstance.post(`/users/4/videos`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       })
       .then(response => {
-        // Fetch the paintings again to update the list
-        fetchPaintings();
+        fetchVideos();
+        form.resetFields();
+        openNotification('success', 'Video added successfully!');
       })
       .catch(error => {
-        console.error('Failed to add painting:', error);
+        console.error('Failed to add video:', error);
+        openNotification('error', 'Failed to add video!');
       });
     }
 
-  const deletePainting = (id) => {
-    // Send a DELETE request to your server to delete a painting
-    // You need to implement this function in your server code
-    axiosInstance.delete(`/users/4/paintings/${id}`)
+  const deleteVideo = (id) => {
+    axiosInstance.delete(`/users/4/videos/${id}`)
       .then(response => {
-        // Fetch the paintings again to update the list
-        fetchPaintings();
+        fetchVideos();
       })
       .catch(error => {
-        console.error('Failed to delete painting:', error);
+        console.error('Failed to delete video:', error);
       });
   }
 
-    const startEditing = (id) => {
-      setEditingId(id);
-    }
+  const startEditing = (id) => {
+    setEditingId(id);
+  }
 
-    const stopEditing = () => {
-      setEditingId(null);
-    }
+  const stopEditing = () => {
+    setEditingId(null);
+  }
 
-    const updatePainting = (values) => {
-      const formData = new FormData();
-      formData.append('title', values.title);
-      formData.append('description', values.description);
-      if (editingFileList.length > 0) {
-        formData.append('image', editingFileList[0].originFileObj);
-      }
-      axiosInstance.put(`/users/4/paintings/${editingId}`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
+  const updateVideo = (values) => {
+    const formData = new FormData();
+    formData.append('title', values.title);
+    formData.append('description', values.description);
+    if (editingFileList.length > 0) {
+      formData.append('video', editingFileList[0].originFileObj);
+    }
+    axiosInstance.put(`/users/4/videos/${editingId}`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+      .then(response => {
+        fetchVideos();
+        setEditingId(null);
+        setEditingFileList([]);
       })
-        .then(response => {
-          fetchPaintings();
-          setEditingId(null);
-          setEditingFileList([]);
-        })
-        .catch(error => {
-          console.error('Failed to update painting:', error);
-        });
-    }
+      .catch(error => {
+        console.error('Failed to update video:', error);
+      });
+  }
 
-  // Fetch the paintings when the component mounts
   useEffect(() => {
-    fetchPaintings();
+    fetchVideos();
   }, []);
 
   return (
     <div>
-      <h2>Your Paintings</h2>
+      <h2>Your Videos</h2>
       <List
         itemLayout="horizontal"
-        dataSource={paintings}
+        dataSource={videos}
         renderItem={item => (
           <List.Item
             actions={[
-              <Button type="primary" onClick={() => deletePainting(item.id)}>Delete</Button>,
+              <Button type="primary" onClick={() => deleteVideo(item.id)}>Delete</Button>,
               editingId === item.id ? (
                 <Button onClick={() => setEditingId(null)}>Cancel</Button>
               ) : (
@@ -125,36 +120,42 @@ const Paintings = () => {
             ]}
           >
             {editingId === item.id ? (
-              <Form form={editingForm} layout="vertical" onFinish={updatePainting}>
-              <Form.Item name="title" label="Title" rules={[{ required: true }]}>
-                <Input />
-              </Form.Item>
-              <Form.Item name="description" label="Description" rules={[{ required: true }]}>
-                <Input.TextArea />
-              </Form.Item>
-              <Form.Item label="Upload">
-                <Upload.Dragger name="file" fileList={editingFileList} onChange={handleEditUpload} beforeUpload={() => false}> 
-                  <p className="ant-upload-drag-icon">
-                    <UploadOutlined />
-                  </p>
-                  <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                </Upload.Dragger>
-              </Form.Item>
-              <Form.Item>
-                <Button type="primary" htmlType="submit">Update Painting</Button>
-              </Form.Item>
-            </Form>
+              <Form form={editingForm} layout="vertical" onFinish={updateVideo}>
+                <Form.Item name="title" label="Title" rules={[{ required: true }]}>
+                  <Input />
+                </Form.Item>
+                <Form.Item name="description" label="Description" rules={[{ required: true }]}>
+                  <Input.TextArea />
+                </Form.Item>
+                <Form.Item label="Upload">
+                  <Upload.Dragger name="file" fileList={editingFileList} onChange={handleEditUpload} beforeUpload={() => false}> 
+                    <p className="ant-upload-drag-icon">
+                      <UploadOutlined />
+                    </p>
+                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                  </Upload.Dragger>
+                </Form.Item>
+                <Form.Item>
+                  <Button type="primary" htmlType="submit">Update Video</Button>
+                </Form.Item>
+              </Form>
             ) : (
-              <List.Item.Meta
-                title={item.title}
-                description={item.description}
-              />
+              <>
+                <List.Item.Meta
+                  title={item.title}
+                  description={item.description}
+                />
+                <Image
+                  width={200}
+                  src={`http://localhost:3000/uploads/${item.image_url}`}
+                />
+              </>
             )}
           </List.Item>
         )}
       />
-      <h2>Add a New Painting</h2>
-      <Form form={form} layout="vertical" onFinish={addPainting}>
+      <h2>Add a New Video</h2>
+      <Form form={form} layout="vertical" onFinish={addVideo}>
         <Form.Item name="title" label="Title" rules={[{ required: true }]}>
           <Input />
         </Form.Item>
@@ -166,7 +167,7 @@ const Paintings = () => {
             name="file" 
             fileList={fileList} 
             onChange={handleUpload} 
-            beforeUpload={() => false} //stop automatic upload when file is selected.
+            beforeUpload={() => false}
             >
             <p className="ant-upload-drag-icon">
                 <UploadOutlined />
@@ -175,12 +176,11 @@ const Paintings = () => {
         </Upload.Dragger>
         </Form.Item>
         <Form.Item>
-          <Button type="primary" htmlType="submit">Add Painting</Button>
+          <Button type="primary" htmlType="submit">Add Video</Button>
         </Form.Item>
       </Form>
     </div>
   );
 };
 
-
-export default Paintings;
+export default Videos;
